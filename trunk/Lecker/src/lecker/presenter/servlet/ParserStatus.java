554a868a19aaabde75a1d0d0c8517451c894495f
stewes89@gmail.com <stewes89@gmail.com@48fc67d3-bf0b-8ac9-5c30-2669e9b416ef>
2013-06-10 22:28:49 +0000

    servlet-mappings hinzugefügt

--- trunk/Lecker/src/lecker/presenter/servlet/ParserStatus.java
+++ trunk/Lecker/src/lecker/presenter/servlet/ParserStatus.java
@@ -2,6 +2,7 @@ package lecker.presenter.servlet;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,6 +13,7 @@ import lecker.presenter.AbstractServlet;
 /**
  * Servlet implementation class ParserStatus
  */
+@WebServlet("/ParserStatus")
 public class ParserStatus extends AbstractServlet {
 	private static final long serialVersionUID = 1L;
        
